f705ed91bf930f9c49e8a7e9830dfeaeb03868fc
Alex Bazlinton <abazlinton@gmail.com>
2017-03-06 03:08:35 +0000

    refactoring!

--- src/Head.js
+++ src/Head.js
@@ -2,29 +2,29 @@ import React from 'react'
 
 const Head = (props) => {
 
-	let containerStyle = {
-		left: props.style.left,
-		top: props.baseTop,
-		width: props.style.width,
-		height: props.style.height,
-		position: "fixed",
-		zIndex: 9,
-		background: "white"
+  let containerStyle = {
+    left: props.style.left,
+    top: props.baseTop,
+    width: props.style.width,
+    height: props.style.height,
+    position: "fixed",
+    zIndex: 9,
+    background: "white"
   } 
   
     return (
-    	<div>
-    		<div 
+      <div>
+        <div 
           className="container"
           style={containerStyle}
         >
         </div>
-	        <img
-	        	className={props.className}
-	        	onClick={props.onWhac}
-	        	style={props.style}
+          <img
+            className={props.className}
+            onClick={props.onWhac}
+            style={props.style}
             id={props.id}
-	        />
+          />
 
         
        </div>
